db78eb38ba5a888fb71ab13d46f2454aeb40e5ad
Karapet Kostandyan <kakostan@microsoft.com>
2022-05-12 09:51:01 +0200

    Revert "Reverted changes related to CompressedPayload. (#1696)"
    This reverts commit 0346ddfd27541ad2d893eb2d71872c7ba12395b2.

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Services/SubscriptionServices.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Services/SubscriptionServices.cs
@@ -947,6 +947,7 @@ namespace Microsoft.Azure.IIoT.OpcUa.Protocol.Services {
                                 EndpointUrl = subscription.Session?.Endpoint?.EndpointUrl,
                                 SubscriptionId = Id,
                                 Timestamp = publishTime,
+                                CompressedPayload = false,
                                 Notifications = new List<MonitoredItemNotificationModel>()
                             };
 
@@ -1022,6 +1023,7 @@ namespace Microsoft.Azure.IIoT.OpcUa.Protocol.Services {
                         EndpointUrl = subscription?.Session?.Endpoint?.EndpointUrl,
                         SubscriptionId = Id,
                         Timestamp = publishTime,
+                        CompressedPayload = false,
                         Notifications = notification.ToMonitoredItemNotifications(
                                 subscription?.MonitoredItems)?.ToList()
                                 ?? new List<MonitoredItemNotificationModel>(),
@@ -1744,6 +1746,7 @@ namespace Microsoft.Azure.IIoT.OpcUa.Protocol.Services {
                     EndpointUrl = Item.Subscription?.Session?.Endpoint?.EndpointUrl,
                     SubscriptionId = (Item.Subscription?.Handle as SubscriptionWrapper)?.Id,
                     Timestamp = DateTime.UtcNow,
+                    CompressedPayload = EventTemplate.PendingAlarms.CompressedPayload,
                     Notifications = new List<MonitoredItemNotificationModel>()
                 };
                 message.Notifications.Add(pendingAlarmsNotification);
